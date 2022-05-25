2e85591ba0044a40ad0868051c9e0abd1405181f
hatsh3p <sarahamarragy@gmail.com>
2022-05-25 10:42:06 -0700

    Reviewed moveZeroes with snowball approach.

---
+++ Arrays/src/MoveZeroes.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        if (nums == null) {
+            return;
+        }
+
+        int count = 0;
+        for (int i = 0; i< nums.length; i++) {
+            if (nums[i] == 0) {
+                ++count;
+            } else if (count > 0) {
+                swap(nums, i, i - count);
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,2,4,0,0,3,0,5,1,0};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums)); // Expected: [1, 3, 12, 0, 0]
+    }
+}
